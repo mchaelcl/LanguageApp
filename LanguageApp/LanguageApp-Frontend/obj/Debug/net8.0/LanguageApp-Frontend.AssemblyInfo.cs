d8b6fd84b0b3443d6f8cf39d412b07ac5bb78050
mchaelcl <65074862+manymike@users.noreply.github.com>
2024-12-03 21:19:16 +0000

    test commit

--- LanguageApp/LanguageApp-Frontend/obj/Debug/net8.0/LanguageApp-Frontend.AssemblyInfo.cs
+++ LanguageApp/LanguageApp-Frontend/obj/Debug/net8.0/LanguageApp-Frontend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LanguageApp-Frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7374306e96b75ca0e6b213642aa9a9a04779002b")]
 [assembly: System.Reflection.AssemblyProductAttribute("LanguageApp-Frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LanguageApp-Frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
