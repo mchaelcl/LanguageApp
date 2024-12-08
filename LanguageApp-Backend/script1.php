2db37ca5d2b8fd6756f2cea93f4d2b493a7816d9
manymike <65074862+manymike@users.noreply.github.com>
2024-12-08 20:25:03 +0000

    php sql get

---
+++ LanguageApp-Backend/script1.php
@@ -0,0 +1,38 @@
+<?php
+
+$user = 'root';
+$password = '';
+$database = 'languageapp';
+$servername = 'localhost';
+
+$mysqli = new mysqli($servername, $user, $password, $database);
+
+if ($mysqli->connect_error) {
+    die("Connect Error " . $mysqli->connect_errno);
+}
+
+// $sql = "INSERT INTO words (english, mandarin, lesson_id) 
+// VALUES ('me', 'wo1', 1)";
+
+$sqlget = "SELECT english, mandarin FROM words";
+$result = $mysqli->query($sqlget);
+
+echo "$result->num_rows";
+
+if($result->num_rows > 0){
+    while($row = $result->fetch_assoc()){
+        echo "english: " . $row["english"]. " - Mandarin " . $row["mandarin"]. "<br>"; 
+    }
+}
+
+else {
+    echo "0 results";
+}
+
+// if ($mysqli->query($sql) === TRUE) {
+//     echo "New record created";
+// } else {
+//     echo "Error: " . $sql . "<br>" . $mysqli->error;
+// }
+
+$mysqli->close();
