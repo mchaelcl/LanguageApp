bb0941c0783d733d2ecefdbc50e7f5f682b2deaf
manymike <65074862+manymike@users.noreply.github.com>
2024-12-11 20:59:33 +0000

    module names for landing encoded as json to send as GET

--- LanguageApp-Backend/script1.php
+++ LanguageApp-Backend/script1.php
@@ -14,22 +14,26 @@ if ($mysqli->connect_error) {
 // $sql = "INSERT INTO words (english, mandarin, lesson_id) 
 // VALUES ('me', 'wo1', 1)";
 
-$sqlget = "SELECT english, mandarin FROM words";
+$sqlget = "SELECT module_title FROM module";
 $result = $mysqli->query($sqlget);
-//turn module info from sql into json array and then create js file to pull and display in html index
-echo json_encode($result);
 
-
-
-if($result->num_rows > 0){
-    while($row = $result->fetch_assoc()){
-        echo "english: " . $row["english"]. " - Mandarin " . $row["mandarin"]. "<br>"; 
-    }
+$resultArray = array();
+while($row = $result->fetch_assoc()){
+    $resultArray[] = $row;
 }
+echo json_encode($resultArray);
 
-else {
-    echo "0 results";
-}
+
+
+// if($result->num_rows > 0){
+//     while($row = $result->fetch_assoc()){
+//         echo "english: " . $row["english"]. " - Mandarin " . $row["mandarin"]. "<br>"; 
+//     }
+// }
+
+// else {
+//     echo "0 results";
+// }
 
 // if ($mysqli->query($sql) === TRUE) {
 //     echo "New record created";
