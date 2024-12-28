91e59d7b8a441a05503ade1e970dba94ae593ec2
manymike <65074862+manymike@users.noreply.github.com>
2024-12-28 22:03:56 +0000

    cors and express setup started

--- LanguageApp-Frontend/script.js
+++ LanguageApp-Frontend/script.js
@@ -1,8 +1,20 @@
 const xhttp = new XMLHttpRequest();
 
-const cors = require('cors');
+const express = require("express");
+const app = express();
 
-appendFile.
+const cors = requires("cors");
+
+app.use(
+cors({
+    origin: "http://127.0.0.1:5500",
+})
+)
+
+app.listen(3000, () => {
+    console
+        .log('Server is running on port 3000');
+});
 
 xhttp.onload = function() {
   console.log("Raw Response:", xhttp.responseText); // Debug the response
